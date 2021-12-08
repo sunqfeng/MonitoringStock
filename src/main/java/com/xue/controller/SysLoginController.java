70e031b38f1e02acb1654ca9d3c0d812ec5093d7
sunqfeng <18722358342@163.com>
2021-12-08 10:59:07 +0800

    add bysqf for 2021年12月8日10:59:04

--- src/main/java/com/xue/controller/SysLoginController.java
+++ src/main/java/com/xue/controller/SysLoginController.java
@@ -37,6 +37,7 @@ public class SysLoginController {
 		System.out.println("hello,world");
 		System.out.println("hello,world");
 		System.out.println("hello,world1111");
+		System.out.println("hello,world1111");
 
 	}
 
