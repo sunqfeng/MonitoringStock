27549a85d662dab7be49fc2cf7bdf44cc8afe94a
sunqfeng <18722358342@163.com>
2021-12-14 14:35:50 +0800

    add by sqf for 2021年12月14日14:35:48.

--- src/main/java/com/xue/repository/dao/SqfSharesMapper.java
+++ src/main/java/com/xue/repository/dao/SqfSharesMapper.java
@@ -28,6 +28,11 @@ public interface SqfSharesMapper {
 
     SqfShares selall_by_securities_code_whether_monitor(@Param("securities_code") String securities_code,@Param("whether_monitor") String whether_monitor );
 
+    /**
+     * 查询所有监控股票的记录
+     * @param whether_monitor
+     * @return
+     */
     List<SqfShares> selall_by_whether_monitor( @Param("whether_monitor") String whether_monitor );
 
     SqfShares selall_by_securities_code_entrusted_price_purchase_entrusted_quantity_purchase( @Param("securities_code") String securities_code,@Param("entrusted_price_purchase") String entrusted_price_purchase,@Param("entrusted_quantity_purchase") String entrusted_quantity_purchase  );
